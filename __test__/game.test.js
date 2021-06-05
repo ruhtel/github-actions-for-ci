42ebd80050510e6325934c02ad4f94eade703f1a
ruhtel <jarrett.ratelle@gmail.com>
2021-06-04 18:30:45 -0700

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,5 +1,5 @@
 const Game = require('../src/game').default
-//const fs = require('fs')
+// const fs = require('fs')
 
 /**
 describe('App', () => {
@@ -11,7 +11,7 @@ describe('App', () => {
     })
   })
 })
-*/
+*/ 
 
 describe('Game', () => {
   let game, p1, p2
